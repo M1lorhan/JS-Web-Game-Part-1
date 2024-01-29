c5d253ce988d34dd3ab96c7f54c1e94703a66228
Marshall Lorhan <mlorhan@gmail.com>
2024-01-28 19:00:04 -0800

    added movement

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ let heightOfGrass = horizon
 tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
 tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
 
-newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
@@ -40,3 +39,67 @@ newImage('assets/well.png', 500, 425)
 newItem('assets/sword.png', 500, 405)
 newItem('assets/shield.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
+
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+if(direction === null){
+    character.src = 'assets/green-character/static.gif'}
+if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'}
+if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'}
+if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'}
+if(direction === 'south'){c
+    haracter.src = 'assets/green-character/south.gif'}
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+function move(character) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'}
+
+    function moveWithArrowKeys(left, bottom, onDirectionChange){
+        let direction = null;
+            let x = left;
+            let y = bottom;
+        
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+            
+            function moveCharacter(){ 
+                if(direction === 'west'){x-=1}
+                if(direction === 'north'){y+=1}
+                if(direction === 'east'){x+=1}
+                if(direction === 'south'){y-=1}
+
+                element.style.left = x + 'px'
+                element.style.bottom = y + 'px'}
+            
+            setInterval(moveCharacter, 1)
+            
+            document.addEventListener('keydown', function(e){
+                if(e.repeat) return;
+            
+                if(e.key === 'ArrowLeft'){direction = 'west'}
+                if(e.key === 'ArrowUp'){direction = 'north'}
+                if(e.key === 'ArrowRight'){direction = 'east'}
+                if(e.key === 'ArrowDown'){direction = 'south'}
+                onDirectionChange(direction)
+            })
+            
+            document.addEventListener('keyup', function(e){
+                direction = null
+                onDirectionChange(direction)})
+        }
+}
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
